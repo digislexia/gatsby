63e06b3397ca50538c7ada5362fe8779f36c7c24
digislexia <digislexia@gmail.com>
2018-06-18 10:23:20 +0300

    add public

---
+++ public/path----9aed3b3a9d2d74ba6dd3.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Gatsby example"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----9aed3b3a9d2d74ba6dd3.js.map
